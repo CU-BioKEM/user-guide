c7f1824b9f8c1c90589fc61213d500cd310cbdc8
shla9937 <shla9937.colorado.edu>
2021-11-24 09:54:38 -0600

    new

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'BioKEM User Guide'
+copyright = '2021, Shawn Laursen'
+author = 'Laursen'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0'
 
 # -- General configuration
 
